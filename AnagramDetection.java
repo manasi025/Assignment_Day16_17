e789c1e3c63ebc82d57098aacd613ab99623b9a7
manasi025 <109495431+manasi025@users.noreply.github.com>
2022-09-19 20:52:34 +0530

    Add files via upload

---
+++ AnagramDetection.java
@@ -0,0 +1,45 @@
+package com.day16_17;
+
+public class AnagramDetection 
+{
+	public static void main(String[] args) 
+	{
+        String firstString = "abcd";
+        String secondString = "dcba";
+
+        if (firstString.length() != secondString.length()) 
+        {
+            System.out.println("Both string are not anagram");
+        }
+        else 
+        {
+            char[] firstArray = firstString.toCharArray();
+            char[] secondArray = secondString.toCharArray();
+
+            anagramDetection(firstArray, secondArray);
+        }
+    }
+    private static void anagramDetection(char[] firstArray, char[] secondArray) 
+    {
+        int count = 0;
+        for (int i = 0; i < firstArray.length; i++) 
+        {
+            for (int j = 0; j < firstArray.length; j++) 
+            {
+                if (firstArray[i] == secondArray[j]) 
+                {
+                    count += 1;
+                }
+            }
+        }
+        if (count == firstArray.length)
+        {
+            System.out.println("Both strings are anagram");
+        }
+        else 
+        {
+            System.out.println("Both strings are not anagram");
+        }
+
+    }
+}
